0ac9bf5cf4eb5c1a9cb67c1780d17a68a10ec2b6
Ricardo <ricardo@ontechies.com>
2014-10-08 08:13:42 -0500

    Initial commit

---
+++ InfiniteScroll/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace InfiniteScroll.Controllers
+{
+    public class HomeController : Controller
+    {
+        AdventureWorksEntities _data = new AdventureWorksEntities();
+        const int recordsPerPage = 8;
+
+        public ActionResult Index(int? id)
+        {
+            var page = id ?? 0;
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Products", GetPaginatedProducts(page));
+            }
+
+            return View("Index", _data.Products.Where(x => x.ProductLine != null).Take(recordsPerPage));
+        }
+
+        public ActionResult Product(int? id)
+        {
+            var page = id ?? 0;
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Products", GetPaginatedProducts(page));
+            }
+
+            return View("Index", _data.Products.Where(x => x.ProductLine != null).Take(recordsPerPage));
+        }
+
+        private List<Product> GetPaginatedProducts(int page = 1)
+        {
+            var skipRecords = page * recordsPerPage;
+
+            var listOfProducts = _data.Products.Where(x => x.ProductLine != null); 
+
+            return listOfProducts.
+                OrderBy(x=>x.Name).
+                Skip(skipRecords).
+                Take(recordsPerPage).ToList();
+        }
+    }
+}
---
+++ InfiniteScroll/Controllers/ImageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace InfiniteScroll.Controllers
+{
+    public class ImageController : Controller
+    {
+        public void Show(int id)
+        {
+            AdventureWorksEntities _data = new AdventureWorksEntities();
+            byte[] image = _data.ProductPhotoes.Where(x => x.ProductPhotoID == id).SingleOrDefault().ThumbNailPhoto;
+
+            Response.Buffer = true;
+
+            Response.Clear();
+
+            Response.ContentType = "image/gif";
+
+            Response.BinaryWrite(image);
+
+            Response.End();
+        }
+    }
+}
